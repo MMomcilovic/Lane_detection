5ab5bea2fa9336ab33ddd4950c434506626c145d
Miladin <miladin.momcilovic@gmail.com>
2023-03-09 12:47:44 +0100

    Improved detection

--- line_detection.py
+++ line_detection.py
@@ -35,135 +35,6 @@ import numpy as np
 from sensor_msgs.msg import Image
 from std_msgs.msg import String
 from cv_bridge import CvBridge
-import matplotlib.pyplot as plt
-
-def middle_lane_point(lines):
-    y_const = 350
-    x_const = 320
-    x_right_list = []
-    x_left_list = []
-
-    x_right = 640
-    x_left = 0
-
-    for line in lines:
-        x1, y1, x2, y2 = line[0]
-        x_check = (x1+x2)/2
-        y_check = (y1+y2)/2
-        check_x = x_check - x_const
-        y_mean = max([y1,y2])
-        if y_mean>250 and y_mean<400 :
-            if check_x>0 and x_check<x_right :
-                _x = int((x_check + x_right)/2)
-                x_right_list.append(_x)
-                x_right = np.average(x_right_list)
-
-            elif check_x<0 and x_check>x_left:
-                _x = int((x_check + x_left)/2)
-                x_left_list.append(_x)
-                x_left = np.average(x_left_list)
-            # error_y = abs(y1-y_check)
-    if len(x_left_list) <= 1:
-        x_left = -100
-    if len(x_right_list) <= 1:
-        x_right = 600
-    x= int((x_right+x_left)/2)
-    return (x, y_const)
-
-def lane_tracking(edges):
-    lines_list =[]
-    lines = cv2.HoughLinesP(
-                edges, # Input edge image
-                2, # Distance resolution in pixels
-                np.pi/180, # Angle resolution in radians
-                threshold=60, # Min number of votes for valid line
-                minLineLength=20, # Min allowed length of line
-                maxLineGap=4# Max allowed gap between line for joining them
-                )
-    # print(lines)
-    for points in lines:
-        # Extracted points nested in the list
-        x1,y1,x2,y2=points[0]
-        lines_list.append([x1,y1,x2,y2])
-    (x,y) = middle_lane_point(lines)
-    return lines_list, (x,y)
-
-def addWeighted(frame, line_image):
-    return cv2.addWeighted(frame, 0.8, line_image, 1, 1)
-
-def display_lines(img,lines):
-    line_image = np.zeros_like(img)
-    if lines is not None:
-        for line in lines:
-            for x1, y1, x2, y2 in line:
-                cv2.line(line_image,(x1,y1),(x2,y2),(0,0,255),10)
-    return line_image
-
-def make_points(image, line):
-    slope, intercept = line
-    y1 = int(image.shape[0])
-    y2 = int(y1*3.0/5)      
-    x1 = int((y1 - intercept)/slope)
-    x2 = int((y2 - intercept)/slope)
-    return [[x1, y1, x2, y2]]
-
-def average_slope_intersect(image, lines):
-    left_fit    = []
-    right_fit   = []
-    if lines is None:
-        return None
-    for line in lines:
-        x1, y1, x2, y2  = line;
-        y_mean = max([y1,y2])
-        if y_mean>250:
-            fit = np.polyfit((x1,x2), (y1,y2), 1)
-            slope = fit[0]
-            intercept = fit[1]
-            if slope < 0: 
-                left_fit.append((slope, intercept))
-            else:
-                right_fit.append((slope, intercept))
-    if (len(left_fit) == 0 or len(right_fit) == 0):
-        return -1
-    left_fit_average  = np.average(left_fit, axis=0)
-    right_fit_average = np.average(right_fit, axis=0)
-    left_line  = make_points(image, left_fit_average)
-    right_line = make_points(image, right_fit_average)
-    averaged_lines = [left_line, right_line]
-    return averaged_lines
-
-def region_of_interest(edges):
-    height = edges.shape[0]
-    width = edges.shape[1]
-    mask = np.zeros_like(edges)
-    triangle = np.array([[(0,height),(320,50), (640, height),]], np.int32)
-    cv2.fillPoly(mask, triangle, 255)
-    masked_image = cv2.bitwise_and(edges, mask)
-    return masked_image
-
-def process_image(image):
-    src_img = cv2.resize(image,(640,480))
-    gray_img = cv2.cvtColor(src_img, cv2.COLOR_BGR2GRAY)
-    ksize = (5, 5)
-    blur_img = cv2.blur(gray_img, ksize, cv2.BORDER_DEFAULT) 
-    edges = cv2.Canny(blur_img,190,230,None, 3)
-    return edges
-
-# def perspective_warp(img,
-#                      dst_size=(640,250),
-#                      src=np.float32([(0,1),(0.95,1),(0,0),(1,0)]),
-#                      dst=np.float32([(0.5,1), (0.6, 1), (0,0), (1,0)])):
-#     img_size = np.float32([(img.shape[1],img.shape[0])])
-#     src = src* img_size
-#     # For destination points, I'm arbitrarily choosing some points to be
-#     # a nice fit for displaying our warped result
-#     # again, not exact, but close enough for our purposes
-#     dst = dst * np.float32(dst_size)
-#     # Given src and dst points, calculate the perspective transform matrix
-#     M = cv2.getPerspectiveTransform(src, dst)
-#     # Warp the image using OpenCV warpPerspective()
-#     warped = cv2.warpPerspective(img, M, dst_size)
-#     return warped
 
 def pipeline(img, s_thresh=(100, 255), sx_thresh=(15, 255)):
     img = np.copy(img)
@@ -172,6 +43,7 @@ def pipeline(img, s_thresh=(100, 255), sx_thresh=(15, 255)):
     l_channel = hls[:,:,1]
     s_channel = hls[:,:,2]
     h_channel = hls[:,:,0]
+
     # Sobel x
     sobelx = cv2.Sobel(l_channel, cv2.CV_64F, 1, 1) # Take the derivative in x
     abs_sobelx = np.absolute(sobelx) # Absolute x derivative to accentuate lines away from horizontal
@@ -186,13 +58,11 @@ def pipeline(img, s_thresh=(100, 255), sx_thresh=(15, 255)):
     s_binary[(s_channel >= s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
     
     color_binary = np.dstack((np.zeros_like(sxbinary), sxbinary, s_binary)) * 255
-    
-    combined_binary = np.zeros_like(sxbinary)
-    combined_binary[(s_binary == 1) | (sxbinary == 1)] = 1
+
     return color_binary
 
 def perspective_warp(img, 
-                     dst_size=(640,170),
+                     dst_size=(640,220),
                      src=np.float32([(0,1),(1,1),(0,0),(1,0)]),
                      dst=np.float32([(0.3,1), (0.7, 1), (0,0), (1,0)])):
     img_size = np.float32([(img.shape[1],img.shape[0])])
@@ -208,7 +78,7 @@ def perspective_warp(img,
     return warped
 
 def inv_perspective_warp(img, 
-                     dst_size=(640,170),
+                     dst_size=(640,220),
                      dst=np.float32([(0,1),(0.95,1),(0,0),(1,0)]),
                      src=np.float32([(0.45,1), (0.6, 1), (0,0), (1,0)])):
     img_size = np.float32([(img.shape[1],img.shape[0])])
@@ -227,20 +97,17 @@ def get_hist(img):
     hist = np.sum(img[img.shape[0]//2:,:], axis=0)
     return hist
 
-left_a, left_b, left_c = [],[],[]
-right_a, right_b, right_c = [],[],[]
 
 def sliding_window(img, nwindows=9, margin=50, minpix = 30, draw_windows=True):
-    global left_a, left_b, left_c,right_a, right_b, right_c 
-    left_fit_= np.empty(3)
-    right_fit_ = np.empty(3)
     out_img = np.dstack((img, img, img))*255
 
-    histogram = get_hist(img[70:170, 0:640])
+    histogram = get_hist(img[100:220, 0:640])
     # find peaks of left and right halves
     midpoint = int(histogram.shape[0]/2)
     leftx_base = np.argmax(histogram[:midpoint])
-    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+    rightx_base = np.argmax(histogram[midpoint + 20:]) + midpoint
+    if rightx_base == 320:
+        rightx_base = 640
     
     
     # Set height of windows
@@ -350,12 +217,7 @@ class CameraHandler():
         rospy.spin()
 
     def callback(self, data):
-        """
-        :param data: sensor_msg array containing the image in the Gazsbo format
-        :return: nothing but sets [cv_image] to the usefull image that can be use in opencv (numpy array)
-        """
         self.cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-        # cv2.imshow("img", perspective_warp(self.cv_image[230:480, 0:640]))
 
         img = self.cv_image
 
@@ -363,47 +225,31 @@ class CameraHandler():
         pipe = pipeline(img)
         # cv2.imshow('first', img)
         # cv2.imshow('pipeline', pipe)
-        perspective = perspective_warp(pipe[330:470, 0:640])
-        # cv2.imshow("perspektiva", perspective)
+        perspective = perspective_warp(pipe[250:470, 0:640])
+        cv2.imshow("perspektiva", perspective)
 
         gray = cv2.cvtColor(perspective, cv2.COLOR_BGR2GRAY)
 
         out_img, curves, lanes, ploty = sliding_window(gray)
-        # cv2.imshow('sliding', out_img)
+        # print(lanes)
+        print('curves', curves)
+        cv2.imshow('sliding', out_img)
         # img_ = draw_lanes(img[300:470, 0:640], curves[0], curves[1])
-        meanx = int(lanes[0][2] + abs(lanes[0][2] - lanes[1][2]) / 2)
+        meanx = int(curves[0][100] + abs(curves[0][100] - curves[1][100]) / 2)
+        if (curves[0][100] == 0 and curves[1][100] == 0):
+            meanx = 320
+        elif meanx < 0:
+            meanx = 0
+        elif meanx > 640:
+            meanx = 640
+        print(meanx)
+        # print(meanx)
         if abs(meanx - self.pre_x) < 20 and abs(meanx - self.pre_x) > 250:
             meanx = self.pre_x
         self.pre_x = meanx
-        image = cv2.circle(self.cv_image, (meanx,300), radius=1, color=(0, 0, 255), thickness=4)
+        image = cv2.circle(self.cv_image, (meanx,350), radius=1, color=(0, 0, 255), thickness=4)
         # cv2.imshow('slicica',img_)
         cv2.imshow('tackica', image)
-
-        # edges = process_image(self.cv_image)
-        # # cv2.imshow("Image with edges",edges)
-        # roi = region_of_interest(edges)
-        # # cv2.imshow("Image with roi",roi)
-        # lines, (x,y) = lane_tracking(roi)
-        # avg_lines = average_slope_intersect(self.cv_image, lines)
-        # if (avg_lines != -1):
-        #     # cv2.imshow("Image with avg",avg_lines)
-        #     line_img = display_lines(self.cv_image, avg_lines)
-        #     # cv2.imshow("Image with line",line_img)
-        #     comboImg = addWeighted(self.cv_image, line_img)
-        #     cv2.imshow("Image with comb",comboImg)
-        #     image = self.cv_image
-        #     if x > 300 and x < 340:
-        #         x = 320
-        #     if abs(x-self.pre_x) < 20:
-        #         x = self.pre_x
-        #     self.pre_x = x
-        #     for line in lines:
-        #         x1,y1,x2,y2=line
-        #         y_mean = max([y1,y2])
-        #         if y_mean>250:
-        #             cv2.line(self.cv_image,(x1,y1),(x2,y2),(0,255,0),2)
-        # image = cv2.circle(self.cv_image, (x,y), radius=1, color=(0, 0, 255), thickness=4)
-        # cv2.imshow("Image with lines",image)
         self.coord_pub.publish(str(meanx))
 
         key = cv2.waitKey(1)
